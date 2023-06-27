7d34b3caf69471134b9863d4ed84cfa7e4b3181b
fadi <fadi99.email@gmail.com>
2023-06-27 23:03:58 +0700

    create post (editor, image upload)

--- src/app/r/[slug]/layout.tsx
+++ src/app/r/[slug]/layout.tsx
@@ -3,6 +3,8 @@ import { db } from "@/lib/db";
 import { notFound } from "next/navigation";
 import { format } from "date-fns";
 import SubscribeLeaveToggle from "@/components/subscribe-leavel-toggle";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -85,6 +87,16 @@ const Layout = async ({ children, params: { slug } }: LayoutProps) => {
                   isSubscribed={isSubscribed}
                 />
               ) : null}
+
+              <Link
+                href={`/r/${slug}/submit`}
+                className={buttonVariants({
+                  variant: "outline",
+                  className: "w-full mb-6",
+                })}
+              >
+                Create Post
+              </Link>
             </dl>
           </div>
         </div>
