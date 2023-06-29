91002bae9120dda8969add1f4e9876dba056acd1
fadi <fadi99.email@gmail.com>
2023-06-29 15:42:52 +0700

    post voting, optimize posts in feed

--- src/components/post-feed.tsx
+++ src/components/post-feed.tsx
@@ -6,7 +6,7 @@ import { useIntersection } from "@mantine/hooks";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { useSession } from "next-auth/react";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import Post from "./post";
 
 interface PostFeedProps {
@@ -41,6 +41,11 @@ const PostFeed = ({ initialPosts, subredditName }: PostFeedProps) => {
     }
   );
 
+  useEffect(() => {
+    if (entry?.isIntersecting) {
+    }
+  }, [entry, fetchNextPage]);
+
   const posts = data?.pages.flatMap((page) => page) ?? initialPosts;
 
   return (
@@ -57,6 +62,8 @@ const PostFeed = ({ initialPosts, subredditName }: PostFeedProps) => {
         );
 
         const props = {
+          currentVote,
+          votesAmt,
           post,
           commentAmt: post.comments.length,
           subredditName: post.subreddit.name,
